17a27242bd72326c6f50c258253a55d5d74d46f9
madhu9613 <rajkhowamadhujya0@gmail.com>
2025-08-19 21:14:32 +0530

    cleaning

--- dp/2ddp/pizza3n.cpp
+++ dp/2ddp/pizza3n.cpp
@@ -1,17 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int solve(int index,int ending,vector<int> slice,int n)
+
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define max3(a, b, c) max(max(a, b), c)
+#define max4(a, b, c, d) max(max(a, b), max(c, d))
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define endl '\n'
+
+ll gcd(ll a, ll b)
 {
-    if()
+    return b == 0 ? a : gcd(b, a % b);
 }
-int main()
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+const int MOD = 1e9 + 7;
+const int INF = INT_MAX;
+const ll LINF = 1e18;
+
+vi slice;
+
+int f(int s,int end,int n)
 {
-    int k;cin>>k;
-    vector<int> slice(k);
-    for(int i=0;i<k;i++)
+    int len=end-s+1;
+vector<vector<int>>dp(len+)
+}
+void solve()
+{int n;cin>>n;
+
+    slice.resize(n);
+    for(int i=0;i<n;i++)
     {
         cin>>slice[i];
     }
+}
 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
     return 0;
 }
